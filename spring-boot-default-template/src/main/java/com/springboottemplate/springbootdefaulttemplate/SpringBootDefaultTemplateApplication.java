6a83d0b79ef7dba5ccb43e72a23203d19b4a48d2
Desmond Beramendi <desmond.bera@gmail.com>
2019-08-25 20:49:58 -0400

    Initialize Spring Boot - Default Template

---
+++ spring-boot-default-template/src/main/java/com/springboottemplate/springbootdefaulttemplate/SpringBootDefaultTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.springboottemplate.springbootdefaulttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDefaultTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDefaultTemplateApplication.class, args);
+	}
+
+}
